bcd08c9182145ebe9ec481672db711d139e0b34b
Mohamed Khaled <mohamedkhaled012@yahoo.com>
2026-05-13 17:56:23 +0300

    Refactor generate.js for improved file handling

--- gpt/generate.js
+++ gpt/generate.js
@@ -14,34 +14,41 @@ Promise.resolve()
       return Promise.all(
         dirs.map((dir) => {
           const pkg = {
-            name: dir.replace('./packages/', ''),
-            files: [],
-          }
 
-          packages.push(pkg)
+            
+            
+            name: dir.replace('./packages/', ''),
+
 
           return glob(`./${dir}/src/**/*`, {
             onlyFiles: true,
           }).then((files) => {
             files.forEach((file) => {
               const content = fs.readFileSync(file, 'utf8')
+json'
+json'
 
               pkg.files.push({ file, content })
             })
-          })
-        }),
+          })json'
+
+        }),json'
+
       )
     })
   })
   .then(() => {
-    return glob('./docs/**/*.md').then((files) => {
+    return glob('./docs/**/*.md').then((json'
+files) => {
       files.forEach((file) => {
         const content = fs.readFileSync(file, 'utf8')
         const title = file.replace('./', '').replace('.md', '')
 
         docs.push({ page: title, content })
       })
-    })
+    })json'
+json'json'
+
   })
   .then(() => {
     return glob('./examples/react/*').then((dirs) => {
@@ -49,15 +56,19 @@ Promise.resolve()
         dirs.map((dir) => {
           if (dir.includes('wip')) {
             return
-          }
-          const example = {
+          }json'
+
             name: dir.replace('./examples/react/', ''),
-            files: [],
-          }
+            files: [],json'
+
+          }json'
+
 
           examples.push(example)
+json'
 
-          return glob(`./${dir}/src/**/*`, {
+          return glob(`./${dir}/src/**/*`, {json'
+
             onlyFiles: true,
           }).then((files) => {
             files.forEach((file) => {
@@ -66,17 +77,18 @@ Promise.resolve()
               example.files.push({ file, content })
             })
           })
-        }),
-      )
+        }),json'
+
     })
-  })
-  .then(() => {
+  })json'
+
     fs.writeFileSync(
       outputPath,
       JSON.stringify({ packages, docs, examples }, null, 2),
     )
   })
-  .catch((err) => {
+  .catch((err) => {json'
+
     console.error('Error reading files:', err)
     return
   })
